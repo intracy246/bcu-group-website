a3dfd1fe24fda69bad830fe3535499f4767ed1bc
intracy246 <winsletsalvatory070@gmail.com>
2026-09-14 13:18:06 +0300

    Allow R2 images on public site

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
+const remotePatterns: NonNullable<NextConfig["images"]>["remotePatterns"] = [];
+
+if (process.env.S3_PUBLIC_URL) {
+  const url = new URL(process.env.S3_PUBLIC_URL);
+  remotePatterns.push({
+    protocol: url.protocol.replace(":", "") as "http" | "https",
+    hostname: url.hostname,
+    pathname: "/**",
+  });
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns,
+  },
 };
 
 export default nextConfig;
